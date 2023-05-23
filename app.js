e9a0ffb3af460fc079d1be22bb15482b0ef06d02
Felix Mandela <felixmandela95@gmail.com>
2023-05-24 00:43:51 +0700

    add layout

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 document.addEventListener("scroll", getNewSection)
-let currPos = window.scrollY
-
+const main = document.getElementById("main")
+let sect = document.getElementsByClassName("section")
 
 function getNewSection() {
     let lastSection = document.querySelector("section:last-child");
     let firstSection = document.querySelector("section:first-child");
+    let wholeSection = document.querySelectorAll("section")
     let lastSectionOffset = lastSection.offsetTop + lastSection.clientHeight;
     let allSection = document.getElementsByClassName("section-link")
     let centerScreen = window.innerHeight / 2
@@ -17,28 +18,69 @@ function getNewSection() {
     // console.log(firstSectionOffset)
     // console.log(window.innerHeight)
     // console.log(firstSection.offsetTop)
-    if (pageOffset > lastSectionOffset - 150) {
-        document.querySelector("main").appendChild(firstSection)
-    }
-    if (pageOffset < firstSectionOffset + 150) {
+    if (pageOffset - 30 < firstSectionOffset) {
+        // main.innerHTML = newHtml + main.innerHTML
         document.querySelector("main").insertBefore(lastSection, firstSection)
     }
-    // currPos = window.scrollY
+    if (pageOffset > lastSectionOffset - 30) {
+        // main.innerHTML += newHtml
+        document.querySelector("main").appendChild(firstSection)
+    }
+
 
     for (let i = 0; i < allSection.length - 1; i++) {
-        console.log(allSection[i].getBoundingClientRect().top)
-        console.log(allSection[i].getBoundingClientRect().top < centerScreen)
-        console.log(centerScreen)
-        console.log(allSection[i].getBoundingClientRect().top + allSection[i].clientHeight)
-        console.log(allSection[i].getBoundingClientRect().top + allSection[i].clientHeight > centerScreen)
-        console.log('-------')
+        // console.log(allSection[i].getBoundingClientRect().top)
+        // console.log(allSection[i].getBoundingClientRect().top < centerScreen)
+        // console.log(centerScreen)
+        // console.log(allSection[i].getBoundingClientRect().top + allSection[i].clientHeight)
+        // console.log(allSection[i].getBoundingClientRect().top + allSection[i].clientHeight > centerScreen)
+        // console.log('-------')
+        // console.log(centerScreen)
+        // console.log(window.scrollY)
         if (allSection[i].getBoundingClientRect().top < centerScreen && allSection[i].getBoundingClientRect().top + allSection[i].clientHeight > centerScreen) {
-            allSection[i].style.color = "black";
-            allSection[i - 1].style.color = "#e8e8e8";
-            allSection[i + 1].style.color = "#e8e8e8";
+            // allSection[i].style.color = "black";
+            // allSection[i - 1].style.color = "#e8e8e8";
+            // allSection[i + 1].style.color = "#e8e8e8";
+
+            // console.log('----')
+            // console.log(allSection[i].offsetTop)
+            // console.log((allSection[i].clientHeight / 2))
+            // console.log(allSection[i].offsetTop + (allSection[i].clientHeight / 2))
+            // console.log(window.scrollY)
+            // console.log('----')
+
+            allSection[i].classList.add('active');
+            allSection[i - 1].classList.remove('active');
+            allSection[i + 1].classList.remove('active');
+        } else {
+            allSection[i].classList.remove('active');
         }
     }
 
 
 }
 
+// for (let i = 0; i < sect.length - 1; i++) {
+//     sect[i].addEventListener("click", () => window.scrollTo(0, 1000))
+// }
+
+for (let i = 0; i < sect.length - 1; i++) {
+    sect[i].addEventListener("click", (e) => {
+        sect = document.getElementsByClassName("section")
+        let scrolly = window.scrollY
+        window.scrollTo(0, e.target.offsetTop - (174 * 2));
+        console.log('---')
+        console.log(e.target)
+        console.log(scrolly)
+        console.log(sect[i].offsetTop)
+        console.log(sect[i].offsetTop + (sect[i].clientHeight / 2))
+        console.log('---')
+    })
+}
+
+function test() {
+    window.scrollTo(0, 300)
+}
+
+// window.onload = test
+// window.addEventListener("load", () => window.scrollTo(0, 0))
