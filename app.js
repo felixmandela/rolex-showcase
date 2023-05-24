75954a49d55baebb8ee3676f15760f5492d82194
Felix Mandela <felixmandela95@gmail.com>
2023-05-25 01:24:33 +0700

    update font and color, add assets

--- app.js
+++ app.js
@@ -1,86 +1,68 @@
-document.addEventListener("scroll", getNewSection)
-const main = document.getElementById("main")
-let sect = document.getElementsByClassName("section")
+const header = document.getElementById("header");
+const linkHeader = document.getElementById("link-container");
+header.addEventListener("scroll", getNewSection);
 
 function getNewSection() {
-    let lastSection = document.querySelector("section:last-child");
-    let firstSection = document.querySelector("section:first-child");
-    let wholeSection = document.querySelectorAll("section")
-    let lastSectionOffset = lastSection.offsetTop + lastSection.clientHeight;
-    let allSection = document.getElementsByClassName("section-link")
-    let centerScreen = window.innerHeight / 2
-    // console.log(lastSection.clientHeight)
-    // console.log(lastSection.offsetTop)
-    // console.log('---')
-    let pageOffset = window.scrollY + window.innerHeight
-    // console.log(pageOffset)
-    let firstSectionOffset = window.innerHeight - firstSection.offsetTop;
-    // console.log(firstSectionOffset)
-    // console.log(window.innerHeight)
-    // console.log(firstSection.offsetTop)
-    if (pageOffset - 30 < firstSectionOffset) {
-        // main.innerHTML = newHtml + main.innerHTML
-        document.querySelector("main").insertBefore(lastSection, firstSection)
-    }
-    if (pageOffset > lastSectionOffset - 30) {
-        // main.innerHTML += newHtml
-        document.querySelector("main").appendChild(firstSection)
+    const body = document.getElementById("body")
+    const lastLink = document.querySelector(".link-wrapper:last-child");
+    const firstLink = document.querySelector(".link-wrapper:first-child");
+    const lastLinkOffset = lastLink.offsetTop + lastLink.clientHeight;
+    const linkWrapper = document.getElementsByClassName("link-wrapper");
+    const headerLink = document.getElementsByClassName("header-link");
+    const imageContainer = document.getElementsByClassName("image-container");
+    const bgColor = document.getElementsByClassName("bg-color");
+    const textColor = document.getElementsByClassName("text-color");
+    const centerScreen = window.innerHeight / 2;
+    const pageOffset = header.scrollTop + window.innerHeight;
+    const firstLinkOffset = window.innerHeight - firstLink.offsetTop;
+
+    if (pageOffset - 10 < firstLinkOffset) {
+        document.querySelector("#link-container").insertBefore(lastLink, firstLink);
     }
 
+    if (pageOffset > lastLinkOffset - 10) {
+        document.querySelector("#link-container").appendChild(firstLink);
+    }
 
-    for (let i = 0; i < allSection.length - 1; i++) {
-        // console.log(allSection[i].getBoundingClientRect().top)
-        // console.log(allSection[i].getBoundingClientRect().top < centerScreen)
-        // console.log(centerScreen)
-        // console.log(allSection[i].getBoundingClientRect().top + allSection[i].clientHeight)
-        // console.log(allSection[i].getBoundingClientRect().top + allSection[i].clientHeight > centerScreen)
-        // console.log('-------')
-        // console.log(centerScreen)
-        // console.log(window.scrollY)
-        if (allSection[i].getBoundingClientRect().top < centerScreen && allSection[i].getBoundingClientRect().top + allSection[i].clientHeight > centerScreen) {
-            // allSection[i].style.color = "black";
-            // allSection[i - 1].style.color = "#e8e8e8";
-            // allSection[i + 1].style.color = "#e8e8e8";
+    let activeIndex = -1;
 
-            // console.log('----')
-            // console.log(allSection[i].offsetTop)
-            // console.log((allSection[i].clientHeight / 2))
-            // console.log(allSection[i].offsetTop + (allSection[i].clientHeight / 2))
-            // console.log(window.scrollY)
-            // console.log('----')
+    for (let i = 0; i < linkWrapper.length; i++) {
+        const linkRect = linkWrapper[i].getBoundingClientRect();
+        if (linkRect.top < centerScreen && linkRect.top + linkRect.height > centerScreen) {
+            activeIndex = i;
+            break;
+        }
+    }
 
-            allSection[i].classList.add('active');
-            allSection[i - 1].classList.remove('active');
-            allSection[i + 1].classList.remove('active');
+
+    for (let i = 0; i < headerLink.length; i++) {
+        if (i === activeIndex) {
+            headerLink[i].classList.add("active");
+            imageContainer[i].style.opacity = "1";
+            body.style.backgroundColor = `${bgColor[i].innerHTML}`;
+            headerLink[i].style.color = `${textColor[i].innerHTML}`
         } else {
-            allSection[i].classList.remove('active');
+            headerLink[i].classList.remove("active");
+            imageContainer[i].style.opacity = "0";
+            imageContainer[i].style.backgroundColor = "";
+            headerLink[i].style.color = "";
         }
     }
 
 
 }
 
-// for (let i = 0; i < sect.length - 1; i++) {
-//     sect[i].addEventListener("click", () => window.scrollTo(0, 1000))
-// }
 
-for (let i = 0; i < sect.length - 1; i++) {
-    sect[i].addEventListener("click", (e) => {
-        sect = document.getElementsByClassName("section")
-        let scrolly = window.scrollY
-        window.scrollTo(0, e.target.offsetTop - (174 * 2));
-        console.log('---')
-        console.log(e.target)
-        console.log(scrolly)
-        console.log(sect[i].offsetTop)
-        console.log(sect[i].offsetTop + (sect[i].clientHeight / 2))
-        console.log('---')
-    })
+
+
+
+
+
+
+
+
+function startScroll() {
+    header.scrollTo(0, 30)
 }
 
-function test() {
-    window.scrollTo(0, 300)
-}
-
-// window.onload = test
-// window.addEventListener("load", () => window.scrollTo(0, 0))
+window.onload = startScroll;
