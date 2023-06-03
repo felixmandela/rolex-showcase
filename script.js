dc12862e385a1c0a2fa3b0edc0ac16d972862d19
Felix Mandela <felixmandela95@gmail.com>
2023-06-03 22:39:49 +0700

    add accessibility code, change variable names to be more clear

--- script.js
+++ script.js
@@ -1,68 +1,72 @@
 const main = document.getElementById("main");
-const linkHeader = document.getElementById("link-container");
+const watchContainer = document.getElementById("watch-container");
+
 window.addEventListener("scroll", getNewSection);
 window.addEventListener("scroll", () => {
-    document.querySelector("#landing-screen").style.opacity = "0"
-    document.querySelector("main").style.opacity = "1"
-    document.querySelector("footer").style.opacity = "1"
+    document.querySelector("#landing-screen").style.opacity = "0";
+    document.querySelector("main").style.opacity = "1";
+    document.querySelector("footer").style.opacity = "1";
 });
 
 function getNewSection() {
-    const body = document.getElementById("body")
-    const lastLink = document.querySelector(".link-wrapper:last-child");
-    const firstLink = document.querySelector(".link-wrapper:first-child");
-    const lastLinkOffset = lastLink.offsetTop + lastLink.clientHeight;
-    const linkWrapper = document.getElementsByClassName("link-wrapper");
-    const headerLink = document.getElementsByClassName("header-link");
-    const imageContainer = document.getElementsByClassName("image-container");
+    const body = document.getElementById("body");
+    const lastWatch = document.querySelector(".watch-item:last-child");
+    const firstWatch = document.querySelector(".watch-item:first-child");
+    const lastWatchOffset = lastWatch.offsetTop + lastWatch.clientHeight;
+    const watchItem = document.getElementsByClassName("watch-item");
+    const watchName = document.getElementsByClassName("watch-name");
+    const watchImageContainer = document.getElementsByClassName("watch-image-container");
     const bgColor = document.getElementsByClassName("bg-color");
     const textColor = document.getElementsByClassName("text-color");
     const centerScreen = window.innerHeight / 2;
     const pageOffset = window.scrollY + window.innerHeight;
-    const firstLinkOffset = window.innerHeight - firstLink.offsetTop;
+    const firstWatchOffset = window.innerHeight - firstWatch.offsetTop;
 
-    // create infinite loop effect
-    // scroll up
-    if (pageOffset - 10 < firstLinkOffset) {
-        document.querySelector("#link-container").insertBefore(lastLink, firstLink);
+    // Create infinite loop effect - scroll up
+    if (pageOffset - 10 < firstWatchOffset) {
+        watchContainer.insertBefore(lastWatch, firstWatch);
     }
-    // scroll down
-    if (pageOffset > lastLinkOffset - 10) {
-        document.querySelector("#link-container").appendChild(firstLink);
+
+    // Scroll down
+    if (pageOffset > lastWatchOffset - 10) {
+        watchContainer.appendChild(firstWatch);
     }
 
     let activeIndex = -1;
-    // select the item on the middle of the screen
-    for (let i = 0; i < linkWrapper.length; i++) {
-        const linkRect = linkWrapper[i].getBoundingClientRect();
-        // move the active criteria to near the bottom of the screen
+
+    // Select the item in the middle of the screen
+    for (let i = 0; i < watchItem.length; i++) {
+        const watchRect = watchItem[i].getBoundingClientRect();
+
+        // Move the active criteria near the bottom of the screen
         if (window.innerWidth <= 625) {
-            if (linkRect.top < window.innerHeight - 150 && linkRect.top + linkRect.height > window.innerHeight - 150) {
+            if (
+                watchRect.top < window.innerHeight - 150 &&
+                watchRect.top + watchRect.height > window.innerHeight - 150
+            ) {
                 activeIndex = i;
                 break;
             }
         } else {
-            if (linkRect.top < centerScreen && linkRect.top + linkRect.height > centerScreen) {
+            if (watchRect.top < centerScreen && watchRect.top + watchRect.height > centerScreen) {
                 activeIndex = i;
                 break;
             }
         }
     }
 
-    // add active class to change styling
-    for (let i = 0; i < headerLink.length; i++) {
+    // Add active class to change styling
+    for (let i = 0; i < watchName.length; i++) {
         if (i === activeIndex) {
-            headerLink[i].classList.add("active");
-            imageContainer[i].style.opacity = "1";
+            watchName[i].classList.add("is-active");
+            watchImageContainer[i].style.opacity = "1";
             body.style.backgroundColor = `${bgColor[i].innerHTML}`;
-            headerLink[i].style.color = `${textColor[i].innerHTML}`
+            watchName[i].style.color = `${textColor[i].innerHTML}`;
         } else {
-            headerLink[i].classList.remove("active");
-            imageContainer[i].style.opacity = "0";
-            imageContainer[i].style.backgroundColor = "";
-            headerLink[i].style.color = "";
+            watchName[i].classList.remove("is-active");
+            watchImageContainer[i].style.opacity = "0";
+            watchImageContainer[i].style.backgroundColor = "";
+            watchName[i].style.color = "";
         }
     }
 }
-
-
